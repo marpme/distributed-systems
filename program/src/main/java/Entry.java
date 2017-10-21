3d7bfe38d6d7bd6478e9cc78065929d4a0e0abae
Marvin Piekarek <cryzar95@googlemail.com>
2017-10-21 15:12:38 +0200

    Cleaned up code

--- program/src/main/java/Entry.java
+++ program/src/main/java/Entry.java
@@ -1,5 +1,3 @@
-import java.io.IOException;
-import java.net.UnknownHostException;
 import java.time.Clock;
 import java.time.LocalDateTime;
 
@@ -7,20 +5,16 @@ import java.time.LocalDateTime;
  * Created by Jan Kulose - s0557320 on 19.10.17.
  */
 public class Entry {
+
     static final String HOST_ADDRESS = "3.ch.pool.ntp.org";
+    static final NTPClient client = new NTPClient();
 
     public static void main(String[] args) {
 
         System.out.println("The current System time is " + LocalDateTime.now(Clock.systemUTC()));
+        String timeResponse = client.getTime(HOST_ADDRESS).toString();
+        System.out.println("The Time Server response is " + timeResponse);
 
-        try {
-            String timeResponse = NTPClient.getTime(HOST_ADDRESS).toString();
-            System.out.println("The Time Server response is " + timeResponse);
-        } catch (UnknownHostException e) {
-            System.out.println("time server not responding");
-        } catch (IOException e) {
-            System.out.println("error reading time");
-        }
 
     }
 
--- program/src/main/java/NTPClient.java
+++ program/src/main/java/NTPClient.java
@@ -1,27 +1,74 @@
 import org.apache.commons.net.ntp.NTPUDPClient;
+import org.apache.commons.net.ntp.NtpV3Packet;
+import org.apache.commons.net.ntp.TimeInfo;
+import org.apache.commons.net.ntp.TimeStamp;
 
 import java.io.IOException;
 import java.net.InetAddress;
 import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
+import java.util.Optional;
+
 
 /**
- * Created by Jan Kulose - s0557320 on 19.10.17.
+ * Wrapper class for the NTPUDPClient (Apache)
  */
 public class NTPClient {
-    private NTPClient() {
+
+    private final NTPUDPClient ntpudpClient;
+
+    public NTPClient() {
+        ntpudpClient = new NTPUDPClient();
     }
 
-    public static LocalDateTime getTime(String hostAddress) throws IOException {
-        // Apache NTP Client
-        NTPUDPClient ntpudpClient = new NTPUDPClient();
+    public NTPClient(NTPUDPClient ntpudpClient) {
+        this.ntpudpClient = ntpudpClient;
+    }
+
+    /**
+     * Receives the Time packet from the chosen NTP server
+     *
+     * @param hostAddress the host address you chose
+     * @return an optional time info
+     */
+    private Optional<TimeInfo> getTimeInfo(String hostAddress) {
+        Optional<TimeInfo> timeInfo = Optional.empty();
+        try {
+            timeInfo = Optional.of(ntpudpClient.getTime(InetAddress.getByName(hostAddress), 123));
+        } catch (IOException e) {
+            System.err.println("Host couldn't be found. Please make sure you entered a correct host.");
+        }
+        return timeInfo;
+    }
+
+    /**
+     * Parses the milliseconds timestamp for the time the packet
+     * was transmitted
+     *
+     * @param timeInfoOptional the received time info packet
+     * @return returns 0 if the server wasn't reachable or a timestamp if he was reachable
+     */
+    private Long getPacketTimestamp(Optional<TimeInfo> timeInfoOptional) {
+        return timeInfoOptional
+                .map(TimeInfo::getMessage)
+                .map(NtpV3Packet::getTransmitTimeStamp)
+                .map(TimeStamp::getTime)
+                .orElse(0L);
+    }
+
+    /**
+     * Receives the current time for a given host
+     *
+     * @param hostAddress the host you chose
+     * @return a LocalDateTime with the time of the host
+     */
+    public LocalDateTime getTime(String hostAddress) {
+
+        // Return the correct time when the packet was transmitted
         return LocalDateTime.ofInstant(
                 // Millisecond epoch time -> Instant
-                Instant.ofEpochMilli(
-                        ntpudpClient.getTime(InetAddress.getByName(hostAddress), 123)
-                        .getMessage().getTransmitTimeStamp().getTime()
-                ),
+                Instant.ofEpochMilli(getPacketTimestamp(getTimeInfo(hostAddress))),
                 // UTC timezone
                 ZoneId.of("UTC")
         );
